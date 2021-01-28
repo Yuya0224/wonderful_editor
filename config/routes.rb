e5b0f177a54009181b63086c80bf00140d789430
yuya <yuya.soccer0224@gmail.com>
2021-01-28 15:16:09 +0900

    task7-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :article_likes
   resources :comments
-  resources :articles
-  mount_devise_token_auth_for "User", at: "auth"
+  namespace :api do
+    namespace :v1 do
+      resources :articles
+      mount_devise_token_auth_for "User", at: "auth"
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
