0a23041777496b7db86d17eb235b9b6e888e44a0
yuya <yuya.soccer0224@gmail.com>
2021-02-08 15:36:56 +0900

    追加

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -48,39 +48,39 @@ RSpec.describe "/comments", type: :request do
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Comment" do
-        expect {
-          post comments_url,
-               params: { comment: valid_attributes }, headers: valid_headers, as: :json
-        }.to change { Comment.count }.by(1)
-      end
+  # describe "POST /create" do
+  #   context "with valid parameters" do
+  #     it "creates a new Comment" do
+  #       expect {
+  #         post comments_url,
+  #              params: { comment: valid_attributes }, headers: valid_headers, as: :json
+  #       }.to change { Comment.count }.by(1)
+  #     end
 
-      # it "renders a JSON response with the new comment" do
-      #   post comments_url,
-      #        params: { comment: valid_attributes }, headers: valid_headers, as: :json
-      #   expect(response).to have_http_status(:created)
-      #   expect(response.content_type).to match(a_string_including("application/json"))
-      # end
-    end
+  #     # it "renders a JSON response with the new comment" do
+  #     #   post comments_url,
+  #     #        params: { comment: valid_attributes }, headers: valid_headers, as: :json
+  #     #   expect(response).to have_http_status(:created)
+  #     #   expect(response.content_type).to match(a_string_including("application/json"))
+  #     # end
+  #   end
 
-    context "with invalid parameters" do
-      it "does not create a new Comment" do
-        expect {
-          post comments_url,
-               params: { comment: invalid_attributes }, as: :json
-        }.to change { Comment.count }.by(0)
-      end
+  #   context "with invalid parameters" do
+  #     it "does not create a new Comment" do
+  #       expect {
+  #         post comments_url,
+  #              params: { comment: invalid_attributes }, as: :json
+  #       }.to change { Comment.count }.by(0)
+  #     end
 
-      # it "renders a JSON response with errors for the new comment" do
-      #   post comments_url,
-      #        params: { comment: invalid_attributes }, headers: valid_headers, as: :json
-      #   expect(response).to have_http_status(:unprocessable_entity)
-      #   expect(response.content_type).to eq("application/json")
-      # end
-    end
-  end
+  #     # it "renders a JSON response with errors for the new comment" do
+  #     #   post comments_url,
+  #     #        params: { comment: invalid_attributes }, headers: valid_headers, as: :json
+  #     #   expect(response).to have_http_status(:unprocessable_entity)
+  #     #   expect(response.content_type).to eq("application/json")
+  #     # end
+  #   end
+  # end
 
   describe "PATCH /update" do
     context "with valid parameters" do
