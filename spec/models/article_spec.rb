eb3f71ebe261823364d2d178f1991296fe670612
yuya <yuya.soccer0224@gmail.com>
2021-02-08 17:46:36 +0900

    task11-1

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,7 @@
 #
 #  id         :bigint           not null, primary key
 #  body       :text
+#  state      :integer
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -21,3 +22,35 @@
 # RSpec.describe Article, type: :model do
 #   # pending "add some examples to (or delete) #{__FILE__}"
 # end
+
+RSpec.describe Article, type: :model do
+  context "titleとbodyがあるとき" do
+    let(:article) { build(:article) }
+
+    it "下書きの記事が作成される" do
+      expect(article).to be_valid
+
+      expect(article.state).to eq "draft"
+      # binding.pry
+    end
+  end
+
+  context "stateが0のとき" do
+    let(:article) { build(:article, state: 0) }
+    it "下書きの状態である" do
+      expect(article).to be_valid
+      expect(article.state).to eq "draft"
+      # binding.pry
+    end
+  end
+
+  context "stateが1のとき" do
+    let(:article) { build(:article, state: 1) }
+
+    it "公開の状態である" do
+      expect(article).to be_valid
+      expect(article.state).to eq "release"
+      # binding.pry
+    end
+  end
+end
